c73d7a9331f02a93e3fc4dd58895e27649f4c1e5
Nekro <70316327+NeKroFR@users.noreply.github.com>
2024-01-12 14:59:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 cd /tmp
 git clone https://github.com/NeKroFR/epita-cfg.git
+rm -rf  ~/.config
+unzip config.zip
+mv cfg/ ~/.config
+mv config /home/louis.beaucamps/.config/i3mv config /home/louis.beaucamps/.config/i3
+mv bashrc /home/louis.beaucamps/.bashrc
